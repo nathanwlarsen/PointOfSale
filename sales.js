e3e98f8a62db1dfc88dbba022fede5f2ef08da7c
Nathan Larsen <nlarsen2@mail.csuchico.edu>
2016-10-31 16:56:02 -0700

    all of lab 18 implemented

--- sales.js
+++ sales.js
@@ -5,15 +5,27 @@ function addItem()
   var newItem;
   newItem = document.getElementById("price").value;
   //IF newItem is not a number
-  // THEN show an alert: "Enter price as a number"
+  if(isNaN(newItem))
+  {
+    // THEN show an alert: "Enter price as a number"
+    window.alert("Enter price as a number");
+  }
   //OTHERWISE,
   // update newItem to its value cast as a number
+  else {
+    newItem = Number(newItem);
+  }
   // update runningTotal to be its value plus newItem
+  runningTotal += newItem;
   // create a variable called dollars
   // call asCurrency() by with the value of runningTotal and assign the return value to dollars
+  var dollars = asCurrency(runningTotal);
   // update the innerHTML of the span with the id "subtotal" to be dollars
+  document.getElementById("subtotal").innerHTML = dollars;
   // update the value of the input with the id "price" to be an empty string
+  document.getElementById("price").value = "";
   // update a cookie called "preTax" with the value of runningTotal
+  setCookie("preTax", runningTotal, 1);
 }
 
 //takes a number and gives a string with the number displayed as USD currency
@@ -24,23 +36,23 @@ function asCurrency(val)
 
 //courtesy of w3schools, from: http://www.w3schools.com/js/js_cookies.asp
 function setCookie(cname, cvalue, exdays) {
-    var d = new Date();
-    d.setTime(d.getTime() + (exdays*24*60*60*1000));
-    var expires = "expires="+d.toUTCString();
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+  var d = new Date();
+  d.setTime(d.getTime() + (exdays*24*60*60*1000));
+  var expires = "expires="+d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 }
 //courtesy of w3schools, from: http://www.w3schools.com/js/js_cookies.asp
 function getCookie(cname) {
-    var name = cname + "=";
-    var ca = document.cookie.split(';');
-    for(var i = 0; i < ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == ' ') {
-            c = c.substring(1);
-        }
-        if (c.indexOf(name) == 0) {
-            return c.substring(name.length, c.length);
-        }
+  var name = cname + "=";
+  var ca = document.cookie.split(';');
+  for(var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
     }
-    return "";
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
 }
